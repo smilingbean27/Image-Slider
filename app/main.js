27c0600381fa589fdfecd744f1bf1157cc947b2a
Lokesh <lokeshisrani1996@gmail.com>
2020-09-22 14:16:44 +0530

    Added feature of Automatic sliding of Images

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@
 var sliderButtons = document.querySelectorAll(".slider_button");
 var sliderImages = document.querySelectorAll(".slider_image");
 
-var current = 0;
+var current = -1;
 var previous = -1;
 
 for (let i= 0; i < sliderButtons.length; i++){
@@ -15,15 +15,33 @@ for (let i= 0; i < sliderButtons.length; i++){
         current = i; 
         console.log(`${i+1} Button is clicked. Current: ${current+1}, Previous: ${previous+1}`);
 
-        sliderImages[current].classList.add("image-animation");
-        sliderImages[current].style.display = "block";
-
-        sliderImages[current].classList.remove("image-animation");
-        sliderImages[previous].style.display = "none";
-
+        imageSlidingIn();
     });
 }
  
+function imageSlidingIn(){
+    sliderImages[current].classList.add("image-animation");
+    sliderImages[current].style.display = "block";
+    
+    if (previous > 0){
+        sliderImages[previous].classList.remove("image-animation");
+        sliderImages[previous].style.display = "none";
+    }
+    
+}
+
+function autoImageSlidingIn(){
+    previous = current;
+    current = (current >= sliderImages.length -1)? 0 : current+1;
+
+    console.log(`Current: ${current+1}, Previous: ${previous+1}`);
+    imageSlidingIn();
+    sliderButtons[current].checked = true;
+    if (previous >0) sliderButtons[previous].checked = false;
+}
+
+autoImageSlidingIn();
+setInterval(autoImageSlidingIn, 5000);
     
 
 
